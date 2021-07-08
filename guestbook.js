8f260edd8fbe33a6d436a05e3059074f33538afb
yashjain0112 <yash.199301055@muj.manipal.edu>
2021-07-08 09:01:10 +0000

    Added UPI route

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a70b44cf.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
